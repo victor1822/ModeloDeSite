940b9ef58436e2043dc1f8570d05ae08ca127080
Victor <victsales@gmail.com>
2020-06-25 20:05:23 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+window.onscroll = function() {
+    const em = document.querySelector('body').style.fontSize;
+    const header = document.querySelector('header');
+    var top = window.scrollY;
+    if ( top > 100 && top < ( 2*window.innerHeight - 4.1*em)){
+        header.classList.add('active');
+    }
+    else{
+        header.classList.remove('active');
+    }
+}
