991c0d15a1865a38a5cd76ebd9251fe7ce324d08
Victor <victsales@gmail.com>
2020-06-26 15:19:44 -0300

    Modificações específicas do projeto do site de letícia

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
     const em = document.querySelector('body').style.fontSize;
     const header = document.querySelector('header');
     var top = window.scrollY;
-    if ( top > 100 && top < ( 2*window.innerHeight - 4.1*em)){
+    if ( top > 100 && top < ( 1*window.innerHeight - 4.1*em)){
         header.classList.add('active');
     }
     else{
